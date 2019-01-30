517f28382083a5dcd7795b578eeffe5f33d64f92
kpobozhny <k.pobozhny@gmail.com>
2019-01-31 00:58:18 +0200

    test arrays, objects

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -26,3 +26,38 @@ describe('greet', () => {
         expect(result).toContain('Mosh');
     });
 }); 
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+        expect(result).toEqual(expect.arrayContaining(['USD','AUD','EUR']));
+        
+    });
+}); 
+
+describe('getProduct', () => {
+    it('should return the product with the given id', () => {
+        const result = lib.getProduct(1);
+        //expect(result).toEqual({id: 1, price: 10});
+        expect(result).toMatchObject({id: 1, price: 10});
+        expect(result).toHaveProperty('id');
+        expect(result).toHaveProperty('id', 1);
+    });
+}); 
+
+describe('registerUser', () => {
+    it('should throw if username is falsy', () => {
+        // in array we set all the values, which are considered falsy in javascript
+        const args = [null, undefined, NaN, '', 0, false];
+        args.forEach(a =>{
+            expect(() => {lib.registerUser(a)}).toThrow();
+        });
+    });
+
+    it('should return a userobject if a valid username is passed', () => {
+        const result = lib.registerUser('mosh');
+        expect(result).toMatchObject({username: 'mosh'});
+        expect(result.id).toBeGreaterThan(0);
+
+    });
+}); 
